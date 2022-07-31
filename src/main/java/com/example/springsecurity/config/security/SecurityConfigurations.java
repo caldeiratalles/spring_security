d9c74a589668c0ff69c8e0426cd52673e3bfe6e9
TM <64755991+caldeiratalles@users.noreply.github.com>
2022-07-30 23:27:56 -0300

    Config Spring Security

---
+++ src/main/java/com/example/springsecurity/config/security/SecurityConfigurations.java
@@ -0,0 +1,70 @@
+package com.example.springsecurity.config.security;
+
+import br.com.devSoluctions.anubis.config.repository.UserRepository;
+import br.com.devSoluctions.anubis.security.AutenticacaoViaTokenFilter;
+import br.com.devSoluctions.anubis.service.security.AutenticacaoService;
+import br.com.devSoluctions.anubis.service.security.TokenService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    private final AutenticacaoService autenticacaoService;
+
+    private final TokenService tokenService;
+
+
+    private final UserRepository usuarioRepository;
+
+    public SecurityConfigurations(AutenticacaoService autenticacaoService, TokenService tokenService, UserRepository usuarioRepository) {
+        this.autenticacaoService = autenticacaoService;
+        this.tokenService = tokenService;
+        this.usuarioRepository = usuarioRepository;
+    }
+
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    //Configuracoes de autenticacao
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    //Configuracoes de autorizacao
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "**/**").hasRole("ADMIN")
+
+                .anyRequest().authenticated()
+                .and().cors()
+                .and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class);
+    }
+
+
+    //Configuracoes de recursos estaticos(js, css, imagens, etc.)
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
+    }
+
+}
